427210b1a6446d0f6016b2a90c0fe06055991685
lmennecke <123320985+lmennecke@users.noreply.github.com>
2025-05-18 23:30:31 +0200

    Update translations.js

--- translations.js
+++ translations.js
@@ -60,7 +60,7 @@ const translations = {
     labelSpielername: "Indique ton nom de joueur et sélectionne tes jeux",
     labelSpielernameInput: "Nom du joueur:",
     placeholderSpielername: "ex. Coureur",
-    labelSpielAuswahl: "À quels jeux jouez-vous?",
+    labelSpielAuswahl: "À quels jeux joues-tu ?",
     optionBitteWaehlen: "Veuillez choisir",
     optionAM: "AM",
     optionSSM: "SSM",
